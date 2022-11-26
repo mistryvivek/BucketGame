1c3d7e207948ce55cab3111b2643e3862f531a64
Vivek <vm800@york.ac.uk>
2022-11-26 13:31:47 +0000

    Fully working example

--- desktop/src/com/badlogic/drop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.badlogic.drop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.drop.drop;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
